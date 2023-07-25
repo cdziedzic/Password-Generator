86f970b47116312955401a7ca0eba5e0dc511826
chris dziedzic <cdziedzic@gmail.com>
2023-07-25 15:33:33 -0400

    added arrays for each required category

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+let lowerAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let upperAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+let special = [" ", "!", '"', "$","#","%","&","(",")"]
+
+
+function generatePassword () {
+  if (confirm("Do you want your password to contain lower case letters?")){
+
+  }
+  
+  if(confirm("Do you want your password to contain uppercase letters?")){
+
+  }
+
+  if (confirm("Do you want your password to contain special characters?")){
+
+  }
+
+  let password = "";
+  let passwordLength =prompt("How many characters do your want your password to be?")
+    for (let i = 0; i < passwordLength ; i++)
+
+    password += "x";
+  
+  return password
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
